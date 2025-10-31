8e9b7c49b2b6fb3fbe2e441bec2cef23371ec2ea
mimomo231 <hieunguyenjr231@gmail.com>
2025-10-31 17:35:28 +0700

    init project

---
+++ config-service/src/test/java/hieu/nguyen/taskflow/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package hieu.nguyen.taskflow.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.integration.dsl.IntegrationFlow;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
---
+++ user-service/src/test/java/hieu/nguyen/taskflow/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.nguyen.taskflow.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
