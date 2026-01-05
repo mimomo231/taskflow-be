80f70277bb8e807206671e872564b8d3f4381926
mimomo231 <hieunguyenjr231@gmail.com>
2026-01-05 11:13:43 +0700

    downgrade spring version to 3.5.6 and delete kafka temporary

--- config-service/src/test/java/hieu/nguyen/taskflow/config_service/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/hieu/nguyen/taskflow/config_service/ConfigServiceApplicationTests.java
@@ -2,7 +2,6 @@ package hieu.nguyen.taskflow.config_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.integration.dsl.IntegrationFlow;
 
 @SpringBootTest
 class ConfigServiceApplicationTests {
